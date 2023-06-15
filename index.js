79dadbba98788f9fa1a90025e7d3e772e5480670
saifion33 <mxofficialplayer@gmail.com>
2023-06-15 18:20:15 +0530

    add some more details to user model createupdate user endpoint

--- index.js
+++ index.js
@@ -19,12 +19,15 @@ const connectDB = async () => {
         await mongoose.connect(process.env.DATABASE_URL);
         console.log(`Database is connected`);
     } catch (error) {
-        console.log(error);
+        console.log('Server not Connected')
+        console.log(error.message);
         process.exit(1);
     }
 }
 
-app.get('/', (req, res) => { res.send('Twitter clone server is working...') })
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'OK' })
+})
 
 app.get('/logged-in-user/:email', async (req, res) => {
     const email = req.params.email;
@@ -48,7 +51,7 @@ app.get('/allTweets', async (req, res) => {
         }
         res.status(404).json({ message: 'tweets not found', data: null })
     } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error', data:null })
+        res.status(500).json({ message: 'Internal Server Error', data: null })
     }
 })
 
@@ -60,16 +63,27 @@ app.post('/newUser', async (req, res) => {
     }
     User.create(req.body).then((response) => res.status(200).json({ message: 'user created successfully.', data: response })).catch(err => { res.send('error creating user' + err) })
 })
+app.patch('/updateUser', async (req, res) => {
+    const { email, update } = req.body;
+    try {
+        const updatedUser = await User.findOneAndUpdate({ email }, update, { new: true })
+        if (updatedUser) {
+            return res.status(200).json({ message: 'User updated successfully', data: updatedUser })
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', data: null })
+    }
+})
 
 app.post('/tweet', async (req, res) => {
-    const { imageUrl, tweet, user} = req.body;
-    Tweet.create({ imageUrl, tweet, user}).then((response) => res.status(200).json({ message: 'Tweet Posted successfully.', data: response })).catch(err => { res.send('error creating tweet' + err) })
+    const { imageUrl, tweet, user } = req.body;
+    Tweet.create({ imageUrl, tweet, user }).then((response) => res.status(200).json({ message: 'Tweet Posted successfully.', data: response })).catch(err => { res.send('error creating tweet' + err) })
 })
 
 //Connect to the database before listening
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`);
-
     })
 })
--- models/user.js
+++ models/user.js
@@ -4,8 +4,13 @@ const schema = new mongoose.Schema({
     name: String,
     email: String,
     userName: String,
+    bio: String,
     id: String,
+    location: String,
     avatarUrl: String,
+    bannerUrl: String,
+    following: [String],
+    followers: [String],
     joinedOn: { type: Date, default: Date.now() }
 });
 
