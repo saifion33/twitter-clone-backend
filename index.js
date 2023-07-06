4dbfadfd515c06690b572c3f4e37336bdc89eb06
Azeem Saifi <100059226+saifion33@users.noreply.github.com>
2023-07-06 18:44:55 +0530

    fix cors origin url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 const app = express()
 const corsOptions = {
-  origin: 'http://localhost:5000/',
+  origin: 'http://localhost:5173/',
   methods: 'GET, POST','PATCH','DELETE', // Specify allowed HTTP methods
   allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
 };
