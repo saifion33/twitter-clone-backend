22917286d65e82940acb076d7bae167dcd87db8c
Azeem Saifi <100059226+saifion33@users.noreply.github.com>
2023-07-06 20:20:21 +0530

    fix cors

--- controllers/user.js
+++ controllers/user.js
@@ -11,7 +11,7 @@ export const getUserById = async (req, res) => {
         }
         res.status(200).json({ message: 'User Found', data: user })
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
         res.status(500).json({ message: 'Internal server Error', data: null })
     }
 }
